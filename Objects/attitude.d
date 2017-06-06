02a84de6109de00ac82e37179c433a63c8f75de2
InfiniteYuan <infiniteyuan@outlook.com>
2017-06-06 17:10:12 +0800

    git init
    add gitignore

--- Objects/attitude.d
+++
@@ -1,6 +0,0 @@
-.\objects\attitude.o: IMU\attitude.cpp
-.\objects\attitude.o: IMU\attitude.h
-.\objects\attitude.o: IMU\Vector3.h
-.\objects\attitude.o: B:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
-.\objects\attitude.o: IMU\Matrix3.h
-.\objects\attitude.o: IMU\Quaternion.h
--- Objects/crc.d
+++
@@ -1,3 +0,0 @@
-.\objects\crc.o: IMU\CRC.cpp
-.\objects\crc.o: IMU\CRC.h
-.\objects\crc.o: B:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
